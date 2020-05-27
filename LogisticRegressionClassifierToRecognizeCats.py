6641d860ef1b60cafda15032b839605d78dd1bf3
Fuze5k <51718249+Fuze5k@users.noreply.github.com>
2020-05-27 12:35:41 +0300

    add dataset

--- LogisticRegressionClassifierToRecognizeCats.py
+++ LogisticRegressionClassifierToRecognizeCats.py
@@ -3,5 +3,10 @@ import matplotlib.pyplot as plt
 import h5py
 #import scipy
 #from PIL import Image
-#from scipy import ndimage
-from lr_utils import load_dataset
+
+def load_dataset():
+    train_dataset = h5py.File('dataset/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    return train_set_x_orig, train_set_y_orig
