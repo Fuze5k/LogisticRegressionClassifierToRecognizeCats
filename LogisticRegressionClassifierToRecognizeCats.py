cd55f62f5736ca1a61d399a0dd8e4d5537b1d29f
Fuze5k <51718249+Fuze5k@users.noreply.github.com>
2020-05-27 18:40:59 +0300

    upgrade code

--- LogisticRegressionClassifierToRecognizeCats.py
+++ LogisticRegressionClassifierToRecognizeCats.py
@@ -22,40 +22,34 @@ class Logic:
         A = self.helper.sigmoid(np.dot(w.T,X) + b)
         dw = (1 / m) * np.dot(X,(A - Y).T)
         db = (1 / m) * np.sum(A - Y,axis=1)
-        grads = {"dw": dw,"db": db}
-        return grads
+        
+        return dw,db
+
 
     def optimize(self, w, b, X, Y, k_iterations, step_learning):
-
         for i in range(k_iterations):
-            grads = self.forward_and_backward_propagate(w,b,X,Y)
-            dw = grads["dw"]
-            db = grads["db"]
+            dw,db = self.forward_and_backward_propagate(w,b,X,Y)
             w = w - step_learning * dw
             b = b - step_learning * db
+        
+        return w,b
 
-        params = {"w": w,"b": b}
-        grads = {"dw": dw,"db": db}
-    
-        return params, grads
 
     def predict(self, w, b, X):
         m = X.shape[1]
         Y_p = np.zeros((1,m))
         w = w.reshape(X.shape[0], 1)
-
         A = self.helper.sigmoid(np.dot(w.T,X) + b)
-   
         for i in range(A.shape[1]):
                 Y_p[0,i] = A[0,i]
 
         return Y_p
    
+
     def training(self, X_train, Y_train, num_iterations=2000, learning_rate=0.5, print_cost=False):
         w, b = self.helper.initialize_w_and_b(X_train.shape[0])
-        parameters, grads = self.optimize(w, b, X_train, Y_train, num_iterations, learning_rate)
-        w = parameters["w"]
-        b = parameters["b"]
+        w,b = self.optimize(w, b, X_train, Y_train, num_iterations, learning_rate)
+        
         return w,b
 
 
@@ -75,8 +69,8 @@ train_set_x = train_set_x_flatten / 255.
 
 num_px = train_set_x_orig.shape[1]
 
-logic = Logic();
-w,b= logic.training(train_set_x, train_set_y,2000, 0.005)
+logic = Logic()
+w,b = logic.training(train_set_x, train_set_y,2000, 0.005)
 
 
 fname = "images/my_image.jpg"
