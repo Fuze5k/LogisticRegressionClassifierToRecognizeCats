3a44eb13f30f9117fa2726b438f75a1256c7ae73
Fuze5k <51718249+Fuze5k@users.noreply.github.com>
2020-05-27 13:37:06 +0300

    add initialize function for w and b

--- LogisticRegressionClassifierToRecognizeCats.py
+++ LogisticRegressionClassifierToRecognizeCats.py
@@ -7,7 +7,11 @@ import h5py
 class Helper:
     def sigmoid(self, f):
         return 1/(1+np.exp(-f))
-
+    
+    def initialize_w_and_b(self, k):
+        w = np.zeros((k,1))
+        b = 0
+        return w, b
 
 
 def load_dataset():
